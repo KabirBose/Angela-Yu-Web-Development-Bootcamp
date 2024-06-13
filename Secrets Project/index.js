51cc2b5d40692fc61a373d443445e6026cd4cd91
Kabir Bose <kabirbose04@gmail.com>
2024-06-13 13:44:23 -0400

    latest

--- Secrets Project/index.js
+++ Secrets Project/index.js
@@ -9,17 +9,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
   let secret;
+  let user;
 
   try {
     const request = await axios.get(
       "https://secrets-api.appbrewery.com/random"
     );
     secret = request.data.secret;
+    user = request.data.username;
   } catch (error) {
     console.error(error.response.data);
   }
 
-  res.render("index.ejs", { secret: secret, user: "larry the lobster" });
+  res.render("index.ejs", { secret, user });
 });
 
 app.listen(3000, () => console.log("Listening on http://localhost:3000"));
